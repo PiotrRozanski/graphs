09902fe55ccb22514adfd2e04f826f797c093cba
Piotr Różański <piotrrozanski1@outlook.com>
2018-05-06 18:34:09 +0200

    fix hamilton algorithm

--- src/app/features/algorithms/hamilton/hamilton.component.ts
+++ src/app/features/algorithms/hamilton/hamilton.component.ts
@@ -3,6 +3,7 @@ import Stack from 'ts-data.stack';
 import {GraphSingleton} from '../../../components/graph-visualization/singletons/GraphSingleton';
 import {Node} from '../../../components/graph-visualization/d3/models';
 import {GraphModel} from '../../../components/graph_model/GraphModel';
+import {isLineBreak} from 'codelyzer/angular/sourceMappingVisitor';
 
 @Component({
   selector: 'app-hamilton',
@@ -30,11 +31,12 @@ export class HamiltonComponent implements OnInit {
   }
 
   public run(num: string) {
-    this.algorytmPath = '';
-    this.result = '';
-    this.isRunAlgorithm = false;
     this.algorithmResult = '';
+    this.result = '';
+    this.algorytmPath = '';
+    this.isRunAlgorithm = false;
     this.isRoot = true;
+
     this.getAllNodeWithGraph();
     const node: Node = this.graph.findNodeBy(Number(num));
     this.DFSHamilton(node);
@@ -45,35 +47,61 @@ export class HamiltonComponent implements OnInit {
 
     this.checkRoot(node);
     const adjacencyNodes = this.graph.getAdjacencyNodes(node);
+    //
     this.stackOfNodes.push(node);
+
     if (this.result !== '') {
       this.result += '->';
     }
+
     this.result += Number(node.id);
 
     if (this.stackOfNodes.count() < this.graph.nodes.length) {
       test = false;
-      this.visited[Number(node.id) - 1] = true;
+      const tmp = Number(node.id) - 1;
+      this.visited.set(node, true);
+
+      // console.log('visited');
+      // console.log(this.visited[Number(node.id) - 1]);
+      // console.log('/');
+      // console.log(this.visited);
 
       adjacencyNodes.forEach((element) => {
-        if (!this.visited[Number(element.id) - 1]) {
+        if (!this.visited.get(element)) {
+          // console.log(element);
           this.DFSHamilton(element);
         }
       });
-      this.visited[Number(node.id) - 1] = false;
+      // console.log('koniec');
+      this.visited.set(node, false);
     } else {
       test = false;
-      adjacencyNodes.forEach((element) => {
-        if (element === this.rootNode) {
+
+      for (const element of adjacencyNodes) {
+        if (element.id === this.rootNode.id) {
           test = true;
+          break;
         }
-      });
-      console.log(test ? this.algorithmResult = 'Cykl Hamiltona:' : this.algorithmResult = 'Sciezka Hamiltona:');
-      console.log(test ? this.result += '->' + Number(this.rootNode.id) + '\r\n' : '\r\n');
-      this.algorytmPath = this.result;
+      }
+      const tmpNodes: Node[] = [];
+      const tmpCount = this.stackOfNodes.count();
+      if (tmpCount !== null) {
+        for (let i = 0; i > tmpCount; i--) {
+          tmpNodes[i] = this.stackOfNodes.pop();
+          // console.log(stackOfNodesKey.valueOf());
+        }
+      }
+      console.log(tmpNodes);
+
+      console.log('end');
+      console.log(test ? 'Cykl Hamiltona:' : 'Sciezka Hamiltona:');
+      console.log(test ? this.result += '->' + Number(this.rootNode.id) + '\r\n' : this.result += '->' + '\r\n');
+      console.log('repeat');
+      // this.algorytmPath += this.algorithmResult + '\n' + this.result + '\n';
       this.isRunAlgorithm = true;
     }
-    this.stackOfNodes = new Stack<Node>();
+     this.stackOfNodes.pop();
+
   }
 
   private getAllNodeWithGraph() {
