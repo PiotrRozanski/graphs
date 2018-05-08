78e9b1838db6c4e776c17703518e42964581277d
Piotr Różański <piotrrozanski1@outlook.com>
2018-05-08 18:30:25 +0200

    refactor

--- src/app/features/algorithms/dfs/TreeNode.ts
+++ src/app/features/algorithms/dfs/TreeNode.ts
@@ -2,11 +2,6 @@ export class TreeNode {
   label: number;
   data: number;
 
-  /**
-   * Creates an instance of TreeNode.
-   * @param {number} label
-   * @param {number} data
-   */
   constructor(label: number, data: number) {
     this.label = label;
     this.data = data;
